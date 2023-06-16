8771f8337990557f21a08ae4e2c8b319e809ea57
Rirzog <prog@Xubuntu.myguest.virtualbox.org>
2023-06-16 17:21:50 -0300

    giving up on conditionals man this is hard

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,9 +1,23 @@
-//function that would randomly return rock, paper or scissors
+//function that returns randomly rock, paper or scissors
+
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+
+
+//function that plays a single round of Rock Paper Scissors.
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You lose paper beats rock";}
+    else if(playerSelection === computerSelection) {
+        return "Tie";}
+        else return "You win rock beats scissors"
+    }
+
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
